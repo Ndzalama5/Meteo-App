4f6e298a05196c3c291e950d1e3fb431779d8231
Ndzalama5 <165193056+Ndzalama5@users.noreply.github.com>
2024-04-06 00:47:32 +0200

    Added icon element

--- src/app.js
+++ src/app.js
@@ -7,14 +7,15 @@ function updateWeather(response) {
   let windElement = document.querySelector("#wind");
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
 
   cityElement.innerHTML = response.data.city;
   timeElement.innerHTML = formatDate(date);
   descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windElement.innerHTML = `${response.data.wind.speed}km/h`;
-
   temperatureElement.innerHTML = Math.round(temperature);
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon"/>`;
 }
 
 function formatDate(date) {
@@ -33,6 +34,10 @@ function formatDate(date) {
 
   if (minutes < 10) {
     minutes = `0${minutes}`;
+  }
+
+  if (hours < 10) {
+    hours = `0${hours}`;
   }
 
   return `${day} ${hours}:${minutes}`;
